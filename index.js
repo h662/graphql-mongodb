3d52b43fa98ad268e36ec8a617d45d7705e7eedc
sh183694@gmail.com <sh183694@gamil.com>
2019-11-06 16:54:00 +0900

    SET mongodb

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // index.js
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
+const dbName = "test";
+const uri =
+  "mongodb+srv://hong:P@ssw0rd@graphql-mongodb-oudhi.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.Promise = global.Promise; // 비동기 처리
+mongoose.connect(uri, { useNewUrlParser: true });
 
 app.get("/", (req, res) => {
   res.json({
