8c2ce4fc4d31894845bdf2bf4757a368f9bb6640
sh183694@gmail.com <sh183694@gamil.com>
2019-11-06 16:49:30 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: "Hello"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listen PORT : ${port}`);
+});
